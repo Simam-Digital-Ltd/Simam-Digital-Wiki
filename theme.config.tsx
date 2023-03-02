fa58f0b1f3a7979b3dc88cb7a9308e569c85826b
Junaid Malik <junmalik109@gmail.com>
2023-03-02 17:54:29 +0000

    Added next config and theme files

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Simam Digital</span>,
+  project: {
+    link: 'https://github.com/Simam-Digital-Ltd/Simam-Digital-Wiki',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/Simam-Digital-Ltd/Simam-Digital-Wiki',
+  footer: {
+    text: 'Simam Digital Wiki',
+  },
+}
+
+export default config
